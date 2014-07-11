db252d8f631c935afb7e60104915a7cd87eafdc0
Qing Wei <wqggu2013@gmail.com>
2014-07-11 00:01:33 -0400

    merge

--- src/hdfs/DataNode/runDataNode.java
+++ src/hdfs/DataNode/runDataNode.java
@@ -4,7 +4,7 @@ import global.Hdfs;
 
 public class runDataNode {
 	public static void main(String[] args) throws InterruptedException {
-		DataNode dataNode1 = new DataNode(Hdfs.NameNode.nameNodeRegistryIP, Hdfs.NameNode.nameNodeRegistryPort, Hdfs.DataNode3.registryPort);
+		DataNode dataNode1 = new DataNode(Hdfs.NameNode.nameNodeRegistryIP, Hdfs.NameNode.nameNodeRegistryPort, Hdfs.DataNode4.registryPort);
 		dataNode1.init();
 		Thread t1 = new Thread(dataNode1);
 		t1.start();
