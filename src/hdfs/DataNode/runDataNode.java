81332157420425d7cee081dae6c16952de94d360
Jeremy Fu <gengf@andrew.cmu.edu>
2014-07-13 21:38:28 -0400

    test getNearest() method

--- src/hdfs/DataNode/runDataNode.java
+++ src/hdfs/DataNode/runDataNode.java
@@ -9,23 +9,23 @@ public class runDataNode {
 		Thread t1 = new Thread(dataNode1);
 		t1.start();
 		
-		Thread.sleep(1000 * 2);
-		DataNode dataNode2 = new DataNode(Hdfs.NameNode.nameNodeRegistryIP, Hdfs.NameNode.nameNodeRegistryPort, Hdfs.DataNode3.registryPort);
-		dataNode2.init();
-		Thread t2 = new Thread(dataNode2);
-		t2.start();
-		
-		Thread.sleep(1000 * 2);
-		DataNode dataNode3 = new DataNode(Hdfs.NameNode.nameNodeRegistryIP, Hdfs.NameNode.nameNodeRegistryPort, Hdfs.DataNode2.registryPort);
-		dataNode3.init();
-		Thread t3 = new Thread(dataNode3);
-		t3.start();
-		
-		Thread.sleep(1000 * 2);
-		DataNode dataNode4 = new DataNode(Hdfs.NameNode.nameNodeRegistryIP, Hdfs.NameNode.nameNodeRegistryPort, Hdfs.DataNode1.registryPort);
-		dataNode4.init();
-		Thread t4 = new Thread(dataNode4);
-		t4.start();
+//		Thread.sleep(1000 * 2);
+//		DataNode dataNode2 = new DataNode(Hdfs.NameNode.nameNodeRegistryIP, Hdfs.NameNode.nameNodeRegistryPort, Hdfs.DataNode3.registryPort);
+//		dataNode2.init();
+//		Thread t2 = new Thread(dataNode2);
+//		t2.start();
+//		
+//		Thread.sleep(1000 * 2);
+//		DataNode dataNode3 = new DataNode(Hdfs.NameNode.nameNodeRegistryIP, Hdfs.NameNode.nameNodeRegistryPort, Hdfs.DataNode2.registryPort);
+//		dataNode3.init();
+//		Thread t3 = new Thread(dataNode3);
+//		t3.start();
+//		
+//		Thread.sleep(1000 * 2);
+//		DataNode dataNode4 = new DataNode(Hdfs.NameNode.nameNodeRegistryIP, Hdfs.NameNode.nameNodeRegistryPort, Hdfs.DataNode1.registryPort);
+//		dataNode4.init();
+//		Thread t4 = new Thread(dataNode4);
+//		t4.start();
 //		
 //		Thread.sleep(1000 * 5);
 //		DataNode dataNode5 = new DataNode(Hdfs.NameNode.nameNodeRegistryIP, Hdfs.NameNode.nameNodeRegistryPort, Hdfs.DataNode5.registryPort);
