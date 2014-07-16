ecf0406df46865af395ac75b336adb913a253779
Jeremy Fu <gengf@andrew.cmu.edu>
2014-07-15 23:21:56 -0400

    Refacor PartitionEntry

--- src/global/MapReduce.java
+++ src/global/MapReduce.java
@@ -15,6 +15,8 @@ public class MapReduce {
 		public static String taskTrackerServiceName = "TaskTracker";
 		public static int MAX_NUM_MAP_TASK = 3;
 		public static int MAX_NUM_REDUCE_TASK = 3;
+		public static int HEART_BEAT_FREQ = 1000; //milliseconds
+		public static int BUFF_SIZE = 1024 * 1024;
 	}
 	
 	public static class TasktTracker1 {
--- src/mapreduce/task/PartitionEntry.java
+++ src/mapreduce/task/PartitionEntry.java
@@ -1,6 +1,36 @@
 package mapreduce.task;
 
 public class PartitionEntry {
-	public TaskTrackerRemoteInterface taskTrackerStub;
-	public String tid;
+	
+	private String tid;
+	
+	private String host;
+	private int port;
+	
+	/**
+	 * 
+	 * @param tid The mapper's tid which is used to located the produced partition
+	 * @param host The TaskTracker's IP on which the mapper runs
+	 * @param port The TaskTracker's Server port
+	 */
+	public PartitionEntry(String tid, String host, int port) {
+
+		this.tid = tid;
+		
+		this.host = host;
+		this.port = port;
+	}
+
+	
+	public String getTID() {
+		return this.tid;
+	}
+	
+	public String getHost() {
+		return this.host;
+	}
+	
+	public int getPort() {
+		return this.port;
+	}
 }
