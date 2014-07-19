3b9dc08aaa5ef058205cd6b75c82986cac8a5195
Qing Wei <wqggu2013@gmail.com>
2014-07-19 15:50:41 -0400

    new example added: nGram

---
+++ src/example/Ngram/NgramReducer.java
@@ -0,0 +1,23 @@
+package example.Ngram;
+
+import java.util.Iterator;
+
+import mapreduce.core.Reducer;
+import mapreduce.io.collector.OutputCollector;
+import mapreduce.io.writable.IntWritable;
+import mapreduce.io.writable.Text;
+
+public class NgramReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+
+	@Override
+	public void reduce(Text key, Iterator<IntWritable> values,
+			OutputCollector<Text, IntWritable> output) {
+		int sum = 0;
+		while (values.hasNext()) {
+			sum++;
+			values.next();
+		}
+		
+		output.collect(key, new IntWritable(sum));
+	}
+}
