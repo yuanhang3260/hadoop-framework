5950a40feb07ef7d62df83fcdaeb327a45da7b1f
Jeremy Fu <gengf@andrew.cmu.edu>
2014-07-21 02:33:23 -0400

    Define the JarFileEntry

--- src/mapreduce/task/JarFileEntry.java
+++ src/mapreduce/task/JarFileEntry.java
@@ -1,5 +1,27 @@
 package mapreduce.task;
 
-public class JarFileEntry {
+import java.io.Serializable;
+
+public class JarFileEntry implements Serializable {	
+	
+	private String taskTrackerIp;
+	
+	private int serverPort;
+	
+	public JarFileEntry (String taskTrackerIp, int serverPort) {
+		
+		this.taskTrackerIp = taskTrackerIp;
+		
+		this.serverPort = serverPort;
+		
+	}
+	
+	public String getTaskTrackerIp() {
+		return this.taskTrackerIp;
+	}
+	
+	public int getServerPort() {
+		return this.serverPort;
+	}
 
 }
