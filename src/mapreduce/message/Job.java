9feba769e1b9c77663793738d41593f7acddc6cd
Jeremy Fu <gengf@andrew.cmu.edu>
2014-07-21 20:28:38 -0400

    track missing files

---
+++ src/mapreduce/message/Job.java
@@ -0,0 +1,59 @@
+package mapreduce.message;
+
+import global.MapReduce;
+
+import java.io.Serializable;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.util.List;
+
+import mapreduce.client.JobConf;
+import mapreduce.io.Split;
+
+public class Job implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -280531931740408893L;
+	private String jobId;
+	private JarFileEntry jarFileEntry;
+	private JobConf conf;
+	private List<Split> splits;
+	
+	public Job(JobConf conf) {
+		this.conf = conf;
+	}
+	
+	public void setJarFileEntry(String jarPath) throws UnknownHostException {
+		this.jarFileEntry = new JarFileEntry(Inet4Address.getLocalHost().getHostAddress(), 
+				 							 MapReduce.TaskTracker.Individual.TASK_TRACKER_SERVER_PORT, 
+				 							 jarPath);
+	}
+	
+	public JarFileEntry getJarFileEntry() {
+		return this.jarFileEntry;
+	}
+	
+	public void setJobId(String id) {
+		this.jobId = id;
+	}
+	
+	public String getJobId() {
+		return this.jobId;
+	}
+	
+	public JobConf getJobConf() {
+		return this.conf;
+	}
+	
+	public void setSplit(List<Split> splits) {
+		this.splits = splits;
+		/* set the number of mapper & reducer tasks */
+		this.conf.setNumMapTasks(splits.size());
+		//this.conf.setNumReduceTasks(splits.size());
+	}
+	
+	public List<Split> getSplit() {
+		return this.splits;
+	}
+}
---
+++ src/mapreduce/message/MapperTask.java
@@ -0,0 +1,71 @@
+package mapreduce.message;
+
+import mapreduce.io.Split;
+
+public class MapperTask extends Task implements MapRedTask{
+	
+	private static final long serialVersionUID = -4448997561951975942L;
+	
+	public Split split;
+	
+	private String mapperClassName;
+	
+	private int partitionNum;
+	
+	private JarFileEntry jarEntry;
+	
+	public MapperTask(String jobId, String tid, int level, Split split, String theClassName, int num, JarFileEntry jarEntry) {
+		super(jobId, tid, level);
+		this.split = split;
+		this.mapperClassName = theClassName;
+		this.partitionNum = num;
+		this.jarEntry = jarEntry;
+	}
+	
+	/**
+	 * Constructor for MapperTask
+	 * @param jobId Job ID
+	 * @param tid Task ID
+	 * @param split The split assigned to mapper
+	 * @param theClass The mapper class submitted by client
+	 * @param num Total partition number
+	 */
+	public MapperTask(String jobId, String tid, Split split, String theClassName, int num, JarFileEntry jarEntry) {
+		super(jobId, tid, 0);
+		this.split = split;
+		this.mapperClassName = theClassName;
+		this.partitionNum = num;
+		this.jarEntry = jarEntry;
+	}
+	
+	public Split getSplit() {
+		return this.split;
+	}
+	
+	public int getPartitionNum() {
+		return this.partitionNum;
+	}
+	
+	public JarFileEntry getJarEntry() {
+		return this.jarEntry;
+	}
+	
+	public void setTaskTrackerLocalJarPath(String localPath) {
+		
+		this.jarEntry.setLocalPath(localPath);
+
+	}
+	
+	public String getMapperClassName() {
+		return this.mapperClassName;
+	}
+	
+
+	@Override
+	public String getJarLocalPath() {
+
+		return this.jarEntry.getLocalPath();
+	
+	}
+
+}
